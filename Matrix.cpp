2428407afb6a76ebc3eaa2acf704a7bbfce1440d
xuboqing <11812910@mail.sustech.edu.cn>
2021-05-25 22:00:02 +0800

    new matrix

--- Matrix.cpp
+++ Matrix.cpp
@@ -6,12 +6,11 @@ Matrix::Matrix(int row, int col) {
     this->col = col;
     vector<double> temp(col);
     this->mat.resize(row, temp);
-   *ptr = mat;
 }
 
 void Matrix::setValue(vector<vector<double>> valueMat) {
     if (valueMat.size() != mat.size() || valueMat[0].size() != mat[0].size()) {
-        cout << "The input value's size does not match the matrix!";
+        cout << "The input value's size does not match the matrix!" << endl;
         exit(0);
     }
     for (int i = 0; i < valueMat.size(); i++) {
@@ -32,7 +31,7 @@ void Matrix::showMatrix() {
 
 Matrix Matrix::operator+(Matrix &a) {
     if (mat.size() != a.mat.size() || mat[0].size() != a.mat[0].size()) {
-        cout << "Matrix size does not match! Plus operation failed.";
+        cout << "Matrix size does not match! Plus operation failed." << endl;
         exit(0);
     }
     Matrix res(row, col);
@@ -50,7 +49,7 @@ Matrix Matrix::operator-(Matrix &a) {
 
 Matrix Matrix::operator/(double a) {
     if (a == 0) {
-        cout << "Divisor can not be zero! Division operation failed.";
+        cout << "Divisor can not be zero! Division operation failed." << endl;
         exit(0);
     }
     Matrix res(row, col);
@@ -99,13 +98,16 @@ Matrix Matrix::conjugation() {
 
 double Matrix::dotProduct(Matrix &other) {
     if (!this->isVector() || !other.isVector()) {
-        cout << "The dot product requires two vectors! Dot product operation failed.";
+        cout << "The dot product requires two vectors! Dot product operation failed." << endl;
+        exit(0);
+    } else if (this->row != other.row) {
+        cout << "The dot product requires two vectors with the same dimensions! Dot product operation failed." << endl;
         exit(0);
     }
-    double res=0;
-        for (int j = 0; j < col; j++) {
-            res+=mat[0][j]+other.mat[0][j];
-        }
+    double res = 0;
+    for (int j = 0; j < col; j++) {
+        res += mat[0][j] * other.mat[0][j];
+    }
     return res;
 }
 
@@ -114,69 +116,120 @@ bool Matrix::isVector() const {
     else return false;
 }
 
-double Matrix::crossProduct(Matrix &other) {
-    return 0;
+Matrix Matrix::crossProduct(Matrix &other) {
+    if (!this->isVector() || !other.isVector()) {
+        cout << "The cross product requires two vectors! Cross product operation failed." << endl;
+        exit(0);
+    } else if (this->row != 3 || other.row != 3) {
+        cout << "The dot product requires two vectors with the 3 dimensions! Dot product operation failed." << endl;
+        exit(0);
+    }
+    Matrix res(row, 1);
+    res.mat[0][0] = mat[0][1] * other.mat[0][2] - mat[0][2] * other.mat[0][1];
+    res.mat[0][1] = mat[0][3] * other.mat[0][0] - mat[0][0] * other.mat[0][3];
+    res.mat[0][2] = mat[0][0] * other.mat[0][1] - mat[0][1] * other.mat[0][0];
+    return res;
 }
 
 double Matrix::det() {
-    Matrix tmp=Matrix(row,col);
-    tmp.ptr=this->ptr;
+    if (row != col) {
+        cout
+                << "The determinant calculation requires a matrix with the equal rows and coloumns! Determinant calculation operation failed."
+                << endl;
+        exit(0);
+    }
+    Matrix tmp = Matrix(row, col);
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             tmp.mat[i][j] = mat[i][j];
         }
     }
-    for(int i=0;i<row-1;i++){
-        if(tmp.mat[i][i]!=0){//对角线元素不为0
-            if(mat[i][i]!=1.0)
-                if(int j=tmp.Find(i,row)){ tmp.Swap(i,j,row);}
-            for(int m=i+1;m<row;m++){
-                if(tmp.mat[m][i]!=0){
-                    float temp=-(tmp.mat[m][i])/tmp.mat[i][i];
-                    for(int p=0;p<row;p++){//R(m)+tempRi
-                        tmp.mat[m][p]=tmp.mat[m][p]+temp*mat[i][p];
+    for (int i = 0; i < row - 1; i++) {
+        if (tmp.mat[i][i] != 0) {
+            if (mat[i][i] != 1.0)
+                if (int j = tmp.Find(i, row)) { tmp.Swap(i, j, row); }
+            for (int m = i + 1; m < row; m++) {
+                if (tmp.mat[m][i] != 0) {
+                    float temp = -(tmp.mat[m][i]) / tmp.mat[i][i];
+                    for (int p = 0; p < row; p++) {
+                        tmp.mat[m][p] = tmp.mat[m][p] + temp * mat[i][p];
                     }
-                }
-                else continue;
+                } else continue;
             }
-        }
-
-        else {//若对角线元素为0
+        } else {
             int m;
-            for(m=i+1;m<row;m++){//
-                if(tmp.mat[m][i]){//使对角线元素非0
-                    for(int p=0;p<row;p++)//Ri+Rm
-                        tmp.mat[i][p]=tmp.mat[m][p]+tmp.mat[i][p];
+            for (m = i + 1; m < row; m++) {
+                if (tmp.mat[m][i]) {
+                    for (int p = 0; p < row; p++)
+                        tmp.mat[i][p] = tmp.mat[m][p] + tmp.mat[i][p];
                     break;
-                }
-                else continue;
+                } else continue;
             }
-            if(m==row){
+            if (m == row) {
                 return 0;
             }
             i--;
         }
     }
 
-    float sum=tmp.mat[0][0];
-    for(int i=1;i<row;i++)
-        sum=sum*tmp.mat[i][i];
+    float sum = tmp.mat[0][0];
+    for (int i = 1; i < row; i++)
+        sum = sum * tmp.mat[i][i];
     return sum;
 
 }
 
-int Matrix::Find( int i, int row) {
-    for(int m=i+1;m<row;m++)
-        if(mat[m][i]==1.0)
+int Matrix::Find(int i, int row) {
+    for (int m = i + 1; m < row; m++)
+        if (mat[m][i] == 1.0)
             return m;
     return 0;
 }
 
 void Matrix::Swap(int i, int j, int row) {
-    for(int m=0;m<row;m++){
-        mat[i][m]+=mat[j][m];
-        mat[j][m]=mat[i][m]-mat[j][m];
-        mat[i][m]-=mat[j][m];
+    for (int m = 0; m < row; m++) {
+        mat[i][m] += mat[j][m];
+        mat[j][m] = mat[i][m] - mat[j][m];
+        mat[i][m] -= mat[j][m];
     }
 }
 
+//void Matrix::setValue(SparseValue values[]) {
+//    for (int i = 0; sizeof(values) / sizeof(int) > i; i++) {
+//        mat[values[i].rowIndex][values[i].colIndex] = values[i].value;
+//    }
+//}
+
+Matrix Matrix::reshape(int newRow, int newCol) {
+    Matrix res(min(this->row, newRow), min(this->col, newCol));
+    for (int i = 0; i < res.row; i++) {
+        for (int j = 0; j < res.col; j++) {
+            res.mat[i][j] = mat[i][j];
+        }
+    }
+    if (this->row > newRow) {
+        if (this->col < newCol) {
+            for (int i = 0; i < res.row; i++) {
+                res.mat[i].insert(res.mat[i].end(), newCol - this->col, 0);
+            }
+            res.col = newCol;
+        }
+    } else if (this->row < newRow) {
+        vector<double> newRows(newCol);
+        res.mat.insert(res.mat.end(), newRow - this->row, newRows);
+        res.row = newRow;
+        if (this->col < newCol) {
+            for (int i = 0; i < res.row; i++) {
+                res.mat[i].insert(res.mat[i].end(), newCol - this->col, 0);
+            }
+            res.col = newCol;
+        }
+    }
+    return res;
+}
+
+SparseValue::SparseValue(int rowIndex, int colIndex, double value) {
+    this->colIndex = colIndex;
+    this->rowIndex = rowIndex;
+    this->value = value;
+}
