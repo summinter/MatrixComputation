bd44ae524893bab4d2d3ef0574ba79b121ba95eb
Summinter <1872800910@qq.com>
2021-06-10 22:04:34 +0800

    加入trace

---
+++ cmake-build-debug/CMakeFiles/MatrixComputation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MatrixComputation.dir/main.cpp.obj"
+  "MatrixComputation.exe"
+  "MatrixComputation.exe.manifest"
+  "MatrixComputation.pdb"
+  "libMatrixComputation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MatrixComputation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
